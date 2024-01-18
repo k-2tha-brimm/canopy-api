45c892b41868cb319b62be73c9f97cc211429f9a
k-2tha-brimm <kev.brimm@gmail.com>
2024-01-18 11:02:00 -0800

    add cors

--- src/server.py
+++ src/server.py
@@ -2,12 +2,27 @@
 from typing import Optional
 
 from fastapi import FastAPI, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel  # pylint: disable=no-name-in-module
 
 from src.knowledge import store_document
 from src.query import query_context
 
 app = FastAPI()
+
+origins = [
+    "https://file-upload-ui.netlify.app/",
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class DocumentPayload(BaseModel):
