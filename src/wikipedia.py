bbc26e282a95bf7ee7bf38a3c98d2e2055503e51
k-2tha-brimm <kev.brimm@gmail.com>
2024-01-24 11:23:17 -0800

    filter words, query by title

--- src/wikipedia.py
+++ src/wikipedia.py
@@ -5,22 +5,26 @@ import requests
 from lxml import html
 from typing import Dict
 
-def fetch_wiki_data(pageid: str) -> Dict:    
+def fetch_wiki_data(title: str) -> Dict:    
     response = requests.get(
     'https://en.wikipedia.org/w/api.php',
     params={
         'action': 'parse',
         'format': 'json',
-        'pageid': pageid,
+        'page': title,
         'prop': 'text',
         'redirects':''
     }).json()
     raw_html = response['parse']['text']['*']
-    title = response['parse']['title']
+    pageid = response['parse']['pageid']
     document = html.document_fromstring(raw_html)
 
     text = ''
     for p in document.xpath('//p'):
-        text += p.text_content() + '\n'
+        stopwords = ['is', 'a', 'at', 'is', 'the']
+        querytext = p.text_content().split()
+
+        result  = [word for word in querytext if word.lower() not in stopwords]
+        text += ' '.join(result)
 
     return {"content": text, "partition_name": f'{title}:{pageid}'}
